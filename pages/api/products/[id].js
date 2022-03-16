2eca57da96bba6c8a971b02b630ef8e091bce117
unknown <jackyjohn1206@gmail.com>
2022-03-16 18:04:08 +0800

    first commit

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -23,9 +23,6 @@ export default async function handler(req, res) {
   }
 
   if (method === "PUT") {
-    if (!token || token !== process.env.token) {
-      return res.status(401).json("Not authenticated!");
-    }
     try {
       const product = await Product.findByIdAndUpdate(id, req.body, {
         new: true,
@@ -37,9 +34,6 @@ export default async function handler(req, res) {
   }
 
   if (method === "DELETE") {
-    if (!token || token !== process.env.token) {
-      return res.status(401).json("Not authenticated!");
-    }
     try {
       await Product.findByIdAndDelete(id);
       res.status(200).json("The product has been deleted!");
